6dc3ff9b4954e807755b385556e322bed2ea229d
htunhtunhtet <ryanhhh91@gmail.com>
2018-05-15 16:46:25 +0800

    axios promises and the github api

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
-var React = require('react');
-var PropTypes = require('prop-types');
+var React       = require('react');
+var PropTypes   = require('prop-types');
+var Link        = require ('react-router-dom').Link;
 
 
 function PlayerPreview (props){
@@ -133,6 +134,8 @@ class Battle extends React.Component {
     }
 
     render(){
+
+        var match       = this.props.match;
         var playerOneName = this.state.playerOneName;
         var playerTwoName = this.state.playerTwoName;
         var playerOneImage= this.state.playerOneImage;
@@ -172,6 +175,17 @@ class Battle extends React.Component {
                     />
                     }
                 </div>
+                {playerOneImage && playerTwoName &&
+                    <Link
+                        className='button'
+                        to={{
+                            pathname: match.url + '/results',
+                            search: `?playerOneName=` + playerOneName + '&playerTwoName=' + playerTwoName
+                        }}
+                    >
+                        Battle
+                    </Link>
+                }
             </div>
         )
     }
